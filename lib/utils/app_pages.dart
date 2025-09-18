252864a3bc05709a1fa0da2316f0fd55be562f72
MuhammadHaris2200 <harisarif809@gmail.com>
2025-09-18 02:18:38 -0700

    office

--- lib/utils/app_pages.dart
+++ lib/utils/app_pages.dart
@@ -7,28 +7,14 @@ import 'package:flutter/cupertino.dart';
 
 import '../view/homeScreen.dart';
 
-class AppPages{
-  static Map<String,WidgetBuilder> getRoutes(){
-    return{
+class AppPages {
+  static Map<String, WidgetBuilder> getRoutes() {
+    return {
       AppRoutes.splash: (context) => const Splashscreen(),
       AppRoutes.onBoarding: (context) => const OnboardingScreen(),
       AppRoutes.login: (context) => const Loginscreen(),
       AppRoutes.signUp: (context) => const Signup(),
       AppRoutes.home: (context) => const Homescreen(),
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     };
   }
 }
